8ef2a267dc0a288f2613733b1cfe618544b7f1bc
Sebastian Frysztak <sebfry@gmail.com>
2019-02-19 13:34:46 +0100

    SeanceController: add grouping

--- src/controller/SeanceController.ts
+++ src/controller/SeanceController.ts
@@ -3,6 +3,14 @@ import { NextFunction, Request, Response } from "express";
 import { Seance } from "../entity/Seance";
 import moment from "moment";
 
+export interface Seances {
+  movieId: number;
+  cinemaId: number;
+  today: Seance[];
+  tomorrow: Seance[];
+  later: Seance[];
+}
+
 export class SeanceController {
   private seanceRepository = getRepository(Seance);
 
@@ -10,11 +18,34 @@ export class SeanceController {
     return this.seanceRepository.find();
   }
 
+  private groupSeances(
+    list: Seance[]
+  ): Pick<Seances, "today" | "tomorrow" | "later"> {
+    const now = moment();
+    const twoDaysAfter = now.clone().add(2, "days");
+
+    const today = list.filter(seance => moment(seance.date).isSame(now, "day"));
+    const tomorrow = list.filter(
+      seance =>
+        moment(seance.date).isAfter(now, "day") &&
+        moment(seance.date).isBefore(twoDaysAfter, "day")
+    );
+    const later = list.filter(seance =>
+      moment(seance.date).isAfter(twoDaysAfter, "day")
+    );
+
+    return {
+      today: today,
+      tomorrow: tomorrow,
+      later: later
+    };
+  }
+
   async upcoming(request: Request, response: Response, next: NextFunction) {
     const movieId = request.params.movieId;
     const cinemaId = request.params.cinemaId;
 
-    return this.seanceRepository.find({
+    const seances = await this.seanceRepository.find({
       where: {
         "movie.multikinoId": movieId,
         "cinema.multikinoId": cinemaId,
@@ -22,5 +53,8 @@ export class SeanceController {
       },
       order: { date: "ASC" }
     });
+
+    const groups = this.groupSeances(seances);
+    return { movieId: movieId, cinemaId: cinemaId, ...groups };
   }
 }
