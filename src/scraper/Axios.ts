0154a5f93b6268139194c1d35400dd1af357bacd
Sebastian Frysztak <sebfry@gmail.com>
2019-03-12 14:59:46 +0100

    axios: throttle requests

--- src/scraper/Axios.ts
+++ src/scraper/Axios.ts
@@ -1,30 +1,34 @@
-require('dotenv').load();
-import axios, { AxiosInstance } from 'axios';
-import * as tunnel from 'tunnel';
+require("dotenv").load();
+import axios, { AxiosInstance } from "axios";
+import { throttleAdapterEnhancer } from "axios-extensions";
+import * as tunnel from "tunnel";
 
-export let axiosClient: AxiosInstance
+export let axiosClient: AxiosInstance;
 
-const proxy = process.env['https_proxy']
+const proxy = process.env["https_proxy"];
+const adapter = throttleAdapterEnhancer(axios.defaults.adapter, {
+  threshold: 2 * 1000
+});
 
 if (proxy) {
-    const url = new URL(proxy)
-    const agent = tunnel.httpsOverHttp({
-        proxy: {
-            host: url.hostname,
-            port: parseInt(url.port),
-            headers: []
-        },
-    });
+  const url = new URL(proxy);
+  const agent = tunnel.httpsOverHttp({
+    proxy: {
+      host: url.hostname,
+      port: parseInt(url.port),
+      headers: []
+    }
+  });
 
-    axiosClient = axios.create({
-        httpsAgent: agent,
-        baseURL: "https://multikino.pl:443",
-        proxy: false
-    });
+  axiosClient = axios.create({
+    httpsAgent: agent,
+    baseURL: "https://multikino.pl:443",
+    proxy: false,
+    adapter: adapter
+  });
 } else {
-    axiosClient = axios.create({ baseURL: "https://multikino.pl" })
+  axiosClient = axios.create({
+    baseURL: "https://multikino.pl",
+    adapter: adapter
+  });
 }
-
-
-
-
